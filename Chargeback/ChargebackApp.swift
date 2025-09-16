c5ebbc8ef03664de9019c7bc2e73551ac70a5a95
Hassan Izhar <hassanizharahmed@hotmail.com>
2025-09-16 16:34:08 +0300

    Initial Commit

---
+++ Chargeback/ChargebackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargebackApp.swift
+//  Chargeback
+//
+//  Created by Hassan Izhar on 16/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ChargebackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chargeback/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chargeback
+//
+//  Created by Hassan Izhar on 16/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
